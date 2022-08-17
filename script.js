0e801425ea8c83f8bf5f61a05a1181f4976873af
felipe <fdomingueskt@gmail.com>
2022-08-17 16:55:27 -0300

    agoravai

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let nome="Felipe"
+let sobrenome="Domingues"
+
+let anoquenasci= 2004;
+let anoatual= 2022;
+let idade=anoatual-anoquenasci
+
+
+let cidade="Curitiba" 
+
+
+console.log(`Eu sou ${nome} ${sobrenome}, tenho ${idade} e atualmente estou morando em ${cidade}`);
